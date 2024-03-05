50301124efa5d3966ce05ca86e1508324ffc66c1
Ayush Vishwakarma <ayushv371@gmail.com>
2024-03-05 09:42:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const key = "0d0a0aa6aa824bb1a0784628240403";
+const base_url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=Delhi&aqi=yes`;
+
+const btn = document.querySelector(".btn");
+const cityName = document.querySelector(".city-name");
+const inputCity = document.getElementById("city");
+const temp = document.querySelector(".temp");
+const img = document.querySelector(".img");
+
+async function getWeather(city) {
+  const base_url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=yes`;
+  let response = await fetch(base_url);
+  let data = await response.json();
+  temp.innerHTML = `${Math.round(data.current.temp_c)}°C`;
+  img.src = data.current.condition.icon;
+}
+
+btn.addEventListener("click", () => {
+  cityName.innerHTML = inputCity.value;
+  getWeather(inputCity.value);
+  inputCity.value = "";
+});
